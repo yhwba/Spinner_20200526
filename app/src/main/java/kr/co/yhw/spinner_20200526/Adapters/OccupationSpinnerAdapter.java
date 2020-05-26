43aee10a7c5bd45f59701af080b6b1cce177a9c0
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-26 16:40:29 +0900

    getDropDownView를 오버라이딩   => 선택지 목록도 보여줄 모양 설정함

--- app/src/main/java/kr/co/yhw/spinner_20200526/Adapters/OccupationSpinnerAdapter.java
+++ app/src/main/java/kr/co/yhw/spinner_20200526/Adapters/OccupationSpinnerAdapter.java
@@ -49,8 +49,28 @@ public class OccupationSpinnerAdapter extends ArrayAdapter<Occupation> {
         iconImg.setImageResource(data.getImageId());
 
 
-
         return row;
 
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+
+        if (row == null) {
+            row = inf.inflate(R.layout.occupation_spinner_list_item, null);
+        }
+
+        ImageView iconImg = row.findViewById(R.id.iconImg);
+        TextView nameTxt = row.findViewById(R.id.nameTxt);
+
+        Occupation data = mList.get(position);
+
+        nameTxt.setText(data.getName());
+
+//        자바에서 이미지를 id로 셋팅
+        iconImg.setImageResource(data.getImageId());
+
+        return row;
     }
 }
